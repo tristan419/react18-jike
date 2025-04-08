2c5e4fd548286bf3a7f01e3209b03dd6bbd12898
tristan419 <tristanlyk@gmail.com>
2025-04-08 14:27:19 +0800

    initial code

---
+++ src/pages/Layout/index.js
@@ -0,0 +1,9 @@
+const Layout = () => {
+  return (
+    <div>
+      <h1>Layout</h1>
+      <p>This is the layout component.</p>
+    </div>
+  );
+}
+export default Layout
---
+++ src/pages/Login/index.js
@@ -0,0 +1,11 @@
+const Login = () => {
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <p>Please enter your credentials to log in.</p>
+      {/* Add your login form here */}
+    </div>
+  );
+}
+
+export default Login
